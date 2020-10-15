3621c5edf734780a58e1e204494296160ab9c515
RTZ <rtz@rajats-MacBook-Air.local>
2020-10-15 12:13:23 +0530

    Updates

---
+++ precache-manifest.8df889ff96e14b26cc6c65f4622cbf2e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "eaee6bc86910757d12821bf824f30a95",
+    "url": "/twitter/index.html"
+  },
+  {
+    "revision": "55ea9746b13c3eae3d63",
+    "url": "/twitter/static/css/main.485161a4.chunk.css"
+  },
+  {
+    "revision": "31c253a56038bf7d66a4",
+    "url": "/twitter/static/js/2.6a66714b.chunk.js"
+  },
+  {
+    "revision": "698c7d1ab7fc994ffbdcf6ff99e91ff0",
+    "url": "/twitter/static/js/2.6a66714b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "55ea9746b13c3eae3d63",
+    "url": "/twitter/static/js/main.c507d94c.chunk.js"
+  },
+  {
+    "revision": "2fa9113302c7812b769d",
+    "url": "/twitter/static/js/runtime-main.96fc9338.js"
+  }
+]);
